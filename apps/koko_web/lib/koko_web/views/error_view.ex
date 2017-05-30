311a686f88b0f6f46a2ff00215703690167d552c
James Carlson <jxxcarlson@gmail.com>
2017-05-30 15:33:25 -0400

    Add back old error view templates

--- apps/koko_web/lib/koko_web/views/error_view.ex
+++ apps/koko_web/lib/koko_web/views/error_view.ex
@@ -5,4 +5,22 @@ defmodule Koko.Web.ErrorView do
     %{error: message}
   end
 
+  def render("404.json", _assigns) do
+    %{errors: %{detail: "Page not found"}}
+  end
+
+  def render("500.json", _assigns) do
+    %{errors: %{info: "Due to programmer laziness, I cannot say more about this error."}}
+  end
+
+  def render("501.json", stuff) do
+    %{errors: stuff.message}
+  end
+
+  # In case no render clause matches or no
+  # template is found, let's render it as 500
+  def template_not_found(_template, assigns) do
+    render "500.json", assigns
+  end
+
 end
