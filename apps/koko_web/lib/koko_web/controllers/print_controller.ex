d76d5093710efe6397f608c12146de0f34dc109a
James Carlson <jxxcarlson@mac.com>
2017-07-22 11:15:30 -0400

    - Fix print template -- use `...` to get multiline string. - To print a document, first map "`" to "!!aWz!!" in the document text.   On the javascript side, map "!!aWz!!" back to "`".  We have to do this   because multiline javascript strings are bounded by "`".

--- apps/koko_web/lib/koko_web/controllers/print_controller.ex
+++ apps/koko_web/lib/koko_web/controllers/print_controller.ex
@@ -5,18 +5,24 @@ defmodule Koko.Web.PrintController do
 
   plug :put_layout, false
 
+  def fix_html(text) do
+    text
+    |> String.replace("`", "!!aWz!!")
+    |> String.replace("\\", "\\\\")
+  end
+
   def show(conn, %{"id" => id}) do
     document = Repo.get(Document, id)
     qs = conn.query_string
     case qs do
       "text=plain" ->
-        conn |> render("plain.html", document: document)
+        conn |> render("plain.html", text: document.rendered_content)
       "text=adoc" ->
-        conn |> render("asciidoc.html", document: document)
+        conn |> render("asciidoc.html", text: fix_html(document.rendered_content))
       "text=latex" ->
-        conn |> render("latex.html", document: document)
+        conn |> render("latex.html", text: document.rendered_content)
       _ ->
-        conn |> render("plain.html", document: document)
+        conn |> render("plain.html", text: document.rendered_content)
     end
   end
 end
