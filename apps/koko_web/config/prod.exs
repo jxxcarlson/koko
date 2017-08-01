6ba099092646996e999995cbc3cecd5abb12e4d2
James Carlson <jxxcarlson@mac.com>
2017-08-01 01:24:38 -0400

    Configure for web

--- apps/koko_web/config/prod.exs
+++ apps/koko_web/config/prod.exs
@@ -19,6 +19,7 @@ config :koko_web, Koko.Web.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  
 
 
 # Configure your database
