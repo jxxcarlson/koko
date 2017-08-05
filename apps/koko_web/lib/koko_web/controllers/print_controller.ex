1b0bd7ec35b98ea6020d506143500ef322defb68
James Carlson <jxxcarlson@mac.com>
2017-08-05 18:08:20 -0400

    remove logging statements

--- apps/koko_web/lib/koko_web/controllers/print_controller.ex
+++ apps/koko_web/lib/koko_web/controllers/print_controller.ex
@@ -12,27 +12,18 @@ defmodule Koko.Web.PrintController do
   end
 
   def show(conn, %{"id" => id}) do
-    IO.puts "PRINT CONTROLLER, SHOW"
     document = Repo.get(Document, String.to_integer(id))
-    IO.puts "Title: #{document.title}"
-    IO.inspect document.attributes
     text_type = document.attributes["text_type"]
-    IO.puts("text_type: #{text_type}")
     case text_type do
       "plain" ->
-        IO.puts "Branch: PLAIN"
         conn |> render("plain.html", text: document.rendered_content)
       "adoc" ->
-          IO.puts "Branch: ADOC"
         conn |> render("asciidoc.html", text: fix_html(document.content))
       "adoc_latex" ->
-            IO.puts "Branch: ADOC LATEX"
           conn |> render("asciidoc.html", text: fix_html(document.content))
       "latex" ->
-          IO.puts "Branch: LATEX"
         conn |> render("latex.html", text: document.content)
       _ ->
-        IO.puts "Branch: DEFAULT"
         conn |> render("asciidoc.html", text: fix_html(document.content))
     end
   end
