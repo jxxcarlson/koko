3f27db2d1670ef8e41968793c423b189fe10c6cd
James Carlson <jxxcarlson@mac.com>
2017-08-05 11:08:53 -0400

    Fix print command

--- apps/koko_web/lib/koko_web/controllers/print_controller.ex
+++ apps/koko_web/lib/koko_web/controllers/print_controller.ex
@@ -12,14 +12,19 @@ defmodule Koko.Web.PrintController do
   end
 
   def show(conn, %{"id" => id}) do
+    IO.puts "PRINT CONTROLLER, SHOW"
     document = Repo.get(Document, id)
-    qs = conn.query_string
-    case qs do
-      "text=plain" ->
+    IO.puts "Title: #{document.title}"
+    IO.inspect document.attributes
+    text_type = document.attributes["text_type"]
+    IO.puts("text_type: #{text_type}")
+    IO.puts("RC: #{document.rendered_content}")
+    case text_type do
+      "plain" ->
         conn |> render("plain.html", text: document.rendered_content)
-      "text=adoc" ->
+      "adoc" ->
         conn |> render("asciidoc.html", text: fix_html(document.rendered_content))
-      "text=latex" ->
+      "latex" ->
         conn |> render("latex.html", text: document.rendered_content)
       _ ->
         conn |> render("plain.html", text: document.rendered_content)
