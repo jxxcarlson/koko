d32169c1b2cf6c7e38b33c8226582d675edd8277
James Carlson <jxxcarlson@mac.com>
2017-09-06 14:03:21 -0700

    Add title and author when printing

--- apps/koko_web/lib/koko_web/controllers/print_controller.ex
+++ apps/koko_web/lib/koko_web/controllers/print_controller.ex
@@ -5,8 +5,9 @@ defmodule Koko.Web.PrintController do
 
   plug :put_layout, false
 
-  def fix_html(text) do
-    text
+  def fix_html(text, title, author) do
+    IO.puts "ENTER fix_html for #{title}"
+    "== #{title}\n=== by #{author}\n\n++++\n<br><br>\n++++\n\n" <>text
     |> String.replace("`", "!!aWz!!")
     |> String.replace("\\", "\\\\")
   end
@@ -14,17 +15,19 @@ defmodule Koko.Web.PrintController do
   def show(conn, %{"id" => id}) do
     document = Repo.get(Document, String.to_integer(id))
     text_type = document.attributes["text_type"]
+    title = document.title
+    author = document.author_name
     case text_type do
       "plain" ->
         conn |> render("plain.html", text: document.rendered_content)
       "adoc" ->
-        conn |> render("asciidoc.html", text: fix_html(document.content))
+        conn |> render("asciidoc.html", text: fix_html(document.content, title, author ))
       "adoc_latex" ->
-          conn |> render("asciidoc.html", text: fix_html(document.content))
+          conn |> render("asciidoc.html", text: fix_html(document.content, title, author ))
       "latex" ->
         conn |> render("latex.html", text: document.content)
       _ ->
-        conn |> render("asciidoc.html", text: fix_html(document.content))
+        conn |> render("asciidoc.html", text: fix_html(document.content, title, author))
     end
   end
 end
