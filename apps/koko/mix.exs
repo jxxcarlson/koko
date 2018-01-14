e59d189356b5a002a8e2ba97ae6a4a2ace736253
James Carlson <jxxcarlson@mac.com>
2018-01-14 15:40:24 +0800

    nada

--- apps/koko/mix.exs
+++ apps/koko/mix.exs
@@ -27,6 +27,7 @@ defmodule Koko.Mixfile do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_),     do: ["lib"]
 
+
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
