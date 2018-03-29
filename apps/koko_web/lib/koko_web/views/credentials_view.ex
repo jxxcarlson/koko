ddd1dceb30f1e464796d60eaa01e15471a1d4bab
James Carlson <jxxcarlson@mac.com>
2018-03-29 12:49:35 -0400

    aws: 1

--- apps/koko_web/lib/koko_web/views/credentials_view.ex
+++ apps/koko_web/lib/koko_web/views/credentials_view.ex
@@ -1,29 +1,29 @@
 defmodule Koko.Web.CredentialsView do
   use Koko.Web, :view
 
-#
-# def render("credentials.json", %{credentials: credentials}) do
-#   credentials
-# end
+
+def render("credentials.json", %{credentials: credentials}) do
+  credentials
+end
 
 
 def render("error.json", %{error: error}) do
   %{error: error}
 end
 #
-def render("credentials.json", %{credentials: credentials}) do
-  %{ url: credentials.url,
-     credentials: %{
-       acl: credentials.credentials.acl,
-       "x-amz-credential": credentials.credentials.credential,
-       "x-amz-date": credentials.credentials.date,
-       "x-amz-algorithm": "AWS4-HMAC-SHA256",
-       key: credentials.credentials.key,
-       policy: credentials.credentials.policy,
-       "x-amz-signature": credentials.credentials.policy,
-    }
-  }
-end
+# def render("credentials.json", %{credentials: credentials}) do
+#   %{ url: credentials.url,
+#      credentials: %{
+#        acl: credentials.credentials.acl,
+#        "x-amz-credential": credentials.credentials.credential,
+#        "x-amz-date": credentials.credentials.date,
+#        "x-amz-algorithm": "AWS4-HMAC-SHA256",
+#        key: credentials.credentials.key,
+#        policy: credentials.credentials.policy,
+#        "x-amz-signature": credentials.credentials.policy,
+#     }
+#   }
+# end
 
 # (field "x-amz-signature" Json.Decode.string)
 #   (field "x-amz-date" Json.Decode.string)
