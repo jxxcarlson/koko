89c0d205c425975da1eee7dc7ee11321b2186b89
James Carlson <jxxcarlson@mac.com>
2018-03-29 11:59:23 -0400

    Add: apps/koko_web/lib/koko_web/views/credentials_view.ex

---
+++ apps/koko_web/lib/koko_web/views/credentials_view.ex
@@ -0,0 +1,38 @@
+defmodule Koko.Web.CredentialsView do
+  use Koko.Web, :view
+
+#
+# def render("credentials.json", %{credentials: credentials}) do
+#   credentials
+# end
+
+
+def render("error.json", %{error: error}) do
+  %{error: error}
+end
+#
+def render("credentials.json", %{credentials: credentials}) do
+  %{ url: credentials.url,
+     credentials: %{
+       acl: credentials.credentials.acl,
+       "x-amz-credential": credentials.credentials.credential,
+       "x-amz-date": credentials.credentials.date,
+       "x-amz-algorithm": "r",
+       key: credentials.credentials.key,
+       policy: credentials.credentials.policy,
+       "x-amz-signature": credentials.credentials.policy,
+    }
+  }
+end
+
+# (field "x-amz-signature" Json.Decode.string)
+#   (field "x-amz-date" Json.Decode.string)
+#   (field "x-amz-credential" Json.Decode.string)
+#   (field "x-amz-algorithm" Json.Decode.string)
+#   (field "policy" Json.Decode.string)
+#   (field "key" Json.Decode.string)
+#   (field "acl" Json.Decode.string)
+#
+# AWSAccessKeyId: credentials.credentials.AWSAccessKeyId,
+
+end
