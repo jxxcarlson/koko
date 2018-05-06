56af0f798623f5360cdf0becfc26cdab144fb2d2
James Carlson <jxxcarlson@mac.com>
2018-05-06 19:55:16 -0400

    Sender can choose plain or html text emails

--- apps/koko/lib/koko/email.ex
+++ apps/koko/lib/koko/email.ex
@@ -4,13 +4,33 @@ defmodule Koko.Email do
 
   alias Koko.Mailer
 
+
   def email(params) do
+    if params["type"] == "html_text" do
+      email_html(params)
+    else
+      email_plain(params)
+    end
+  end
+
+  def email_html(params) do
     IO.puts "Email.email, sending to #{params["recipient"]}"
     new_email
     |> to(params["recipient"])
     |> from("support@node.io")
     |> subject(params["subject"])
     |> html_body(params["body"])
+    |> Mailer.deliver_now
+  end
+
+
+  def email_plain(params) do
+    IO.puts "Email.email, sending to #{params["recipient"]}"
+    new_email
+    |> to(params["recipient"])
+    |> from("support@node.io")
+    |> subject(params["subject"])
+    |> text_body(params["body"])
     |> Mailer.deliver_now
   end
 
